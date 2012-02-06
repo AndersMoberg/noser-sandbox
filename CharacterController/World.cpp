8e865739a2cbcd2596a7d87e709e35e36c6745e5
Nolan.Check@gmail.com <Nolan.Check@gmail.com@5662e9d3-f524-1b14-0759-363ede264f15>
2012-02-06 21:50:48 +0000

    Adjustments for testing. The problem is that we don't handle more than one wall constraint correctly.

--- CharacterController/World.cpp
+++ CharacterController/World.cpp
@@ -23,5 +23,12 @@ WorldPtr World::Create()
 	wall.end = Vector2f(-5.0f, 0.0f);
 	p->m_walls.push_back(wall);
 
+	wall.start = Vector2f(-10.0f, 8.0f);
+	wall.end = Vector2f(-9.0f, 0.0f);
+	p->m_walls.push_back(wall);
+	wall.start = Vector2f(-9.0f, 0.0f);
+	wall.end = Vector2f(-3.0f, -7.0f);
+	p->m_walls.push_back(wall);
+
 	return p;
 }
