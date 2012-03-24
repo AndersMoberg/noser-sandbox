59d09e898fabc5e1374d0a2f310222fe70f92f69
Nolan.Check@gmail.com <Nolan.Check@gmail.com@5662e9d3-f524-1b14-0759-363ede264f15>
2012-03-24 19:11:23 +0000

    H5N: cleanup

--- HTML5CanvasNurbs/nurbs.js
+++ HTML5CanvasNurbs/nurbs.js
@@ -9,9 +9,14 @@ var Nurbs = (function () {
         var NUM_STEPS = 256;
         for (var i = 0; i < NUM_STEPS; ++i) {
             var t = i / (NUM_STEPS - 1);
-            var denom = w0 * (1 - t) * (1 - t) * (1 - t) + 3 * w1 * (1 - t) * (1 - t) * t + 3 * w2 * (1 - t) * t * t + w3 * t * t * t;
-            var xt = (w0 * cp0x * (1 - t) * (1 - t) * (1 - t) + 3 * w1 * cp1x * (1 - t) * (1 - t) * t + 3 * w2 * cp2x * (1 - t) * t * t + w3 * x * t * t * t) / denom;
-            var yt = (w0 * cp0y * (1 - t) * (1 - t) * (1 - t) + 3 * w1 * cp1y * (1 - t) * (1 - t) * t + 3 * w2 * cp2y * (1 - t) * t * t + w3 * y * t * t * t) / denom;
+			var t2 = t*t;
+			var t3 = t2*t;
+			var u = 1-t;
+			var u2 = u*u;
+			var u3 = u2*u;
+            var denom = w0*u3 + 3*w1*u2*t + 3*w2*u*t2 + w3*t3;
+            var xt = (w0*cp0x*u3 + 3*w1*cp1x*u2*t + 3*w2*cp2x*u*t2 + w3*x*t3) / denom;
+            var yt = (w0*cp0y*u3 + 3*w1*cp1y*u2*t + 3*w2*cp2y*u*t2 + w3*y*t3) / denom;
             context.lineTo(xt, yt);
         }
     };
