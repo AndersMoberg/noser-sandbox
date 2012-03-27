46da379bffd8473630c2f1b3df891688161f7be5
Nolan.Check@gmail.com <Nolan.Check@gmail.com@5662e9d3-f524-1b14-0759-363ede264f15>
2012-03-27 20:44:11 +0000

    GS: cleanup

--- GameSandbox/CharacterTestMode/CharacterTestMode.cpp
+++ GameSandbox/CharacterTestMode/CharacterTestMode.cpp
@@ -79,109 +79,82 @@ private:
 	long m_bottles;
 };
 
-class CharacterTestModeImpl : public CharacterTestMode
-{
-
-public:
-
-	static CharacterTestModeImpl* Create(GameRenderer* renderer)
-	{
-		CharacterTestModeImpl* p(new CharacterTestModeImpl);
-
-		p->m_renderer = renderer;
-
-		p->m_bgTexture.reset(p->m_renderer->GetGLES2Manager()->CreateTextureFromFile(
-			L"C:\\Users\\Public\\Pictures\\Sample Pictures\\Tulips.jpg"));
-
-		p->m_characterTexture.reset(p->m_renderer->GetGLES2Manager()->CreateTextureFromFile(
-			L"C:\\Users\\Public\\Pictures\\Sample Pictures\\Jellyfish.jpg"));
-
-		p->m_object.reset(new MyGameObject(p->m_renderer));
-
-		return p;
-	}
-
-	virtual void Tick(const GameInput& input)
-	{
-		Vector2f velocity = input.move * m_characterSpeed;
-		m_characterPos += velocity / (float)Game::TICKS_PER_SEC;
-
-		m_object->Tick();
-	}
-
-	virtual void Render()
-	{
-		unsigned int width = m_renderer->GetGLES2Manager()->GetWidth();
-		unsigned int height = m_renderer->GetGLES2Manager()->GetHeight();
-
-		glViewport(0, 0, width, height);
-
-		glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		Rectf vp(0.0f, 0.0f, (float)width, (float)height);
-		Matrix3x2f worldToViewport = m_camera.GetWorldToViewport(vp);
-		Matrix3x2f viewportToClip = Matrix3x2f::RectLerp(vp,
-			Rectf(-1.0f, 1.0f, 1.0f, -1.0f));
-		Matrix3x2f worldToClip = worldToViewport * viewportToClip;
-
-		m_renderer->GetGLES2Manager()->SetTexturedQuadMatrix(worldToClip);
-	
-		// Draw background image
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, m_bgTexture->Get());
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		// Premultiplied alpha blending
-		glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-		glBlendEquation(GL_FUNC_ADD);
-		glEnable(GL_BLEND);
-
-		m_renderer->GetGLES2Manager()->DrawTexturedQuad(Rectf(-16.0f, 16.0f, 16.0f, -16.0f));
-
-		// Draw character image
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, m_characterTexture->Get());
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		// Premultiplied alpha blending
-		glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-		glBlendEquation(GL_FUNC_ADD);
-		glEnable(GL_BLEND);
-
-		m_renderer->GetGLES2Manager()->DrawTexturedQuad(m_characterRect.Offset(m_characterPos));
-
-		m_object->Render();
-	}
-
-private:
-
-	CharacterTestModeImpl()
-		: m_characterPos(0.0f, 0.0f),
-		m_characterSpeed(6.0f),
-		m_characterRect(-2.0f, 2.0f, 2.0f, -2.0f)
-	{ }
-
-	GameRenderer* m_renderer;
-
-	Camera m_camera;
-
-	std::unique_ptr<GLES2Texture> m_bgTexture;
-
-	Vector2f m_characterPos;
-	float m_characterSpeed;
-	Rectf m_characterRect;
-	std::unique_ptr<GLES2Texture> m_characterTexture;
-
-	std::unique_ptr<GameObject> m_object;
-
-};
+CharacterTestMode::CharacterTestMode()
+	: m_characterPos(0.0f, 0.0f),
+	m_characterSpeed(6.0f),
+	m_characterRect(-2.0f, 2.0f, 2.0f, -2.0f)
+{ }
 
 CharacterTestMode* CharacterTestMode::Create(GameRenderer* renderer)
 {
-	return CharacterTestModeImpl::Create(renderer);
+	CharacterTestMode* p(new CharacterTestMode);
+
+	p->m_renderer = renderer;
+
+	p->m_bgTexture.reset(p->m_renderer->GetGLES2Manager()->CreateTextureFromFile(
+		L"C:\\Users\\Public\\Pictures\\Sample Pictures\\Tulips.jpg"));
+
+	p->m_characterTexture.reset(p->m_renderer->GetGLES2Manager()->CreateTextureFromFile(
+		L"C:\\Users\\Public\\Pictures\\Sample Pictures\\Jellyfish.jpg"));
+
+	p->m_object.reset(new MyGameObject(p->m_renderer));
+
+	return p;
+}
+
+void CharacterTestMode::Tick(const GameInput& input)
+{
+	Vector2f velocity = input.move * m_characterSpeed;
+	m_characterPos += velocity / (float)Game::TICKS_PER_SEC;
+
+	m_object->Tick();
+}
+
+void CharacterTestMode::Render()
+{
+	unsigned int width = m_renderer->GetGLES2Manager()->GetWidth();
+	unsigned int height = m_renderer->GetGLES2Manager()->GetHeight();
+
+	glViewport(0, 0, width, height);
+
+	glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	Rectf vp(0.0f, 0.0f, (float)width, (float)height);
+	Matrix3x2f worldToViewport = m_camera.GetWorldToViewport(vp);
+	Matrix3x2f viewportToClip = Matrix3x2f::RectLerp(vp,
+		Rectf(-1.0f, 1.0f, 1.0f, -1.0f));
+	Matrix3x2f worldToClip = worldToViewport * viewportToClip;
+
+	m_renderer->GetGLES2Manager()->SetTexturedQuadMatrix(worldToClip);
+	
+	// Draw background image
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, m_bgTexture->Get());
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Premultiplied alpha blending
+	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+	glBlendEquation(GL_FUNC_ADD);
+	glEnable(GL_BLEND);
+
+	m_renderer->GetGLES2Manager()->DrawTexturedQuad(Rectf(-16.0f, 16.0f, 16.0f, -16.0f));
+
+	// Draw character image
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, m_characterTexture->Get());
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Premultiplied alpha blending
+	glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+	glBlendEquation(GL_FUNC_ADD);
+	glEnable(GL_BLEND);
+
+	m_renderer->GetGLES2Manager()->DrawTexturedQuad(m_characterRect.Offset(m_characterPos));
+
+	m_object->Render();
 }
 
 }
--- GameSandbox/CharacterTestMode/CharacterTestMode.hpp
+++ GameSandbox/CharacterTestMode/CharacterTestMode.hpp
@@ -15,10 +15,31 @@ namespace CharacterTestMode
 
 class CharacterTestMode : public GameMode
 {
+
 public:
+
 	static CharacterTestMode* Create(GameRenderer* renderer);
-	virtual void Tick(const GameInput& input) = 0;
-	virtual void Render() = 0;
+
+	virtual void Tick(const GameInput& input);
+	virtual void Render();
+	
+private:
+
+	CharacterTestMode();
+
+	GameRenderer* m_renderer;
+
+	Camera m_camera;
+
+	std::unique_ptr<GLES2Texture> m_bgTexture;
+
+	Vector2f m_characterPos;
+	float m_characterSpeed;
+	Rectf m_characterRect;
+	std::unique_ptr<GLES2Texture> m_characterTexture;
+
+	std::unique_ptr<GameObject> m_object;
+
 };
 
 }
